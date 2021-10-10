947e3c9f43ad219b6ed60f6dc7fbec192bdd3aa0
Burhon <megacoder77@gmail.com>
2021-10-10 19:31:58 +0500

    change modal style

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 3,
   spaceBetween: 20,
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     0: {
       slidesPerView: 1,
@@ -18,41 +23,41 @@ const swiper = new Swiper('.swiper-container', {
   },
 
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
 
-const servicesItem = document.querySelectorAll('.services__item');
-const modalOverlay = document.querySelector('.modal__overlay');
-const modals = document.querySelectorAll('.modal');
-const closeBtn = document.querySelectorAll('.modal__close');
-const consultBtn = document.querySelector('.services__help');
-consultBtn.addEventListener('click', (e) => {
-  const id = consultBtn.closest('.services__inf').dataset.id;
-  document.querySelector(`div[data-id="${id}"]`).classList.add('modal--active');
-  modalOverlay.classList.add('modal__overlay--active');
+const servicesItem = document.querySelectorAll(".services__item");
+const modalOverlay = document.querySelector(".modal__overlay");
+const modals = document.querySelectorAll(".modal");
+const closeBtn = document.querySelectorAll(".modal__close");
+const consultBtn = document.querySelector(".services__help");
+consultBtn.addEventListener("click", (e) => {
+  const id = consultBtn.closest(".services__inf").dataset.id;
+  document.querySelector(`div[data-id="${id}"]`).classList.add("modal--active");
+  modalOverlay.classList.add("modal__overlay--active");
 });
 servicesItem.forEach((item) => {
-  item.addEventListener('click', (e) => {
+  item.addEventListener("click", (e) => {
     document
       .querySelector(`div[data-id="${item.dataset.id}"]`)
-      .classList.add('modal--active');
-    modalOverlay.classList.add('modal__overlay--active');
+      .classList.add("modal--active");
+    modalOverlay.classList.add("modal__overlay--active");
   });
 });
 closeBtn.forEach((close) => {
-  close.addEventListener('click', (e) => {
-    console.log('asfd');
+  close.addEventListener("click", (e) => {
+    console.log("asfd");
     modals.forEach((modal) => {
-      modal.classList.remove('modal--active');
+      modal.classList.remove("modal--active");
     });
-    modalOverlay.classList.remove('modal__overlay--active');
+    modalOverlay.classList.remove("modal__overlay--active");
   });
 });
-modalOverlay.addEventListener('click', (e) => {
-  modalOverlay.classList.remove('modal__overlay--active');
+modalOverlay.addEventListener("click", (e) => {
+  modalOverlay.classList.remove("modal__overlay--active");
   modals.forEach((modal) => {
-    modal.classList.remove('modal--active');
+    modal.classList.remove("modal--active");
   });
 });
